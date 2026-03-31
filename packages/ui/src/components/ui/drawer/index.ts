c7484fd43879487cfddbf5242d712526a9518e8b
zuo <159921208@qq.com>
2026-03-31 08:29:09 +0800

    refactor(ui): encapsulate UI headless dependencies and remove vaul-svelte

--- packages/ui/src/components/ui/drawer/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as Root } from './drawer.svelte';
-export { default as Content } from './drawer-content.svelte';
-export { default as Header } from './drawer-header.svelte';
-export { default as Footer } from './drawer-footer.svelte';
-export { default as Title } from './drawer-title.svelte';
-export { default as Description } from './drawer-description.svelte';
